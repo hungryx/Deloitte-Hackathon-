418144f482fc83f0075f7b9483ff962d43e22f6d
Marcus Lo <m.lo@student.unsw.edu.au>
2020-06-12 22:50:43 +1000

    cal updates

--- codedoc/src/screens/Calendar.js
+++ codedoc/src/screens/Calendar.js
@@ -1,17 +1,45 @@
 import React, { useState } from 'react'
+import { StructuredListWrapper, StructuredListHead, StructuredListRow, StructuredListCell, StructuredListBody } from 'carbon-components-react'
 import CalendarComponent from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 export const Calendar = ({ name = 'Marcus' }) => {
   const [day, setDay] = useState(new Date())
+  const symptoms = [
+    { symptom: 'Fatigue', occurences: 13 },
+    { symptom: 'Headaches', occurences: 8 },
+    { symptom: 'Stomachaches', occurences: 6 }
+  ]
+
   return (
     <div style={{
-      padding: 40
+      padding: 40,
+      display: 'flex'
     }}>
       <div>
         <CalendarComponent onChange={setDay} value={day} />
       </div>
-      <div>
-        
+      <div style={{
+        paddingLeft: 48
+      }}>
+        <h4>Your most frequent symptoms</h4>
+        <StructuredListWrapper>
+          <StructuredListHead>
+            <StructuredListRow head>
+              <StructuredListCell head />
+              <StructuredListCell head>Symptom</StructuredListCell>
+              <StructuredListCell head>Occurences</StructuredListCell>
+            </StructuredListRow>
+          </StructuredListHead>
+          <StructuredListBody>
+            {symptoms.map((symptom, index) => (
+              <StructuredListRow>
+              <StructuredListCell noWrap><b>{index + 1}</b></StructuredListCell>
+              <StructuredListCell>{symptom.symptom}</StructuredListCell>
+              <StructuredListCell>{symptom.occurences}</StructuredListCell>
+            </StructuredListRow>
+            ))}
+          </StructuredListBody>
+        </StructuredListWrapper>
       </div>
     </div>
   )
--- codedoc/src/screens/Welcome.js
+++ codedoc/src/screens/Welcome.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Symptoms, Medication } from '../components'
-export const Welcome = ({ name = 'Marcus' }) => {
+export const Welcome = ({ name = 'Marcus' }) => {      
   const symptoms = [
     { label: 'headache', value: 'headache' },
     { label: 'fatigue', value: 'fatigue' },
