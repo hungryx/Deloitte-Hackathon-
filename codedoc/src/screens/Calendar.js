2eed5c6cab1ef8d9c63f08ce9b2198601acf8cc1
Marcus Lo <m.lo@student.unsw.edu.au>
2020-06-12 23:33:50 +1000

    cal

--- codedoc/src/screens/Calendar.js
+++ codedoc/src/screens/Calendar.js
@@ -1,25 +1,44 @@
 import React, { useState } from 'react'
 import { StructuredListWrapper, StructuredListHead, StructuredListRow, StructuredListCell, StructuredListBody } from 'carbon-components-react'
 import CalendarComponent from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 export const Calendar = ({ name = 'Marcus' }) => {
   const [day, setDay] = useState(new Date())
-  const symptoms = [
+  const symptoms1 = [
     { symptom: 'Fatigue', occurences: 13 },
     { symptom: 'Headaches', occurences: 8 },
     { symptom: 'Stomachaches', occurences: 6 }
   ]
+  
+  const symptoms2 = [
+    { symptom: 'Headaches', occurences: 9 },
+    { symptom: 'Fatigue', occurences: 4 },
+    { symptom: 'Stomachaches', occurences: 2 }
+  ]
+  
+  const symptoms3 = [
+    { symptom: 'Fatigue', occurences: 14 },
+    { symptom: 'Headaches', occurences: 4 },
+    { symptom: 'Stomachaches', occurences: 3 }
+  ]
+  
+  const symptoms = [ symptoms1, symptoms2, symptoms3 ]
+  const [dataSet, setDataSet] = useState(symptoms[0])
 
   return (
     <div style={{
       padding: 40,
-      display: 'flex'
     }}>
-      <div>
-        <CalendarComponent onChange={setDay} value={day} />
-      </div>
+      <center>
+        <CalendarComponent onChange={(date) => {
+          let max = 2
+          let min = 0
+          setDataSet(symptoms[Math.floor(Math.random() * (max - min + 1)) + min])
+          setDay(date)
+        }} value={day} />
+      </center>
       <div style={{
-        paddingLeft: 48
+        marginTop: 48
       }}>
         <h4>Your most frequent symptoms</h4>
         <StructuredListWrapper>
@@ -31,7 +50,7 @@ export const Calendar = ({ name = 'Marcus' }) => {
             </StructuredListRow>
           </StructuredListHead>
           <StructuredListBody>
-            {symptoms.map((symptom, index) => (
+            {dataSet.map((symptom, index) => (
               <StructuredListRow>
               <StructuredListCell noWrap><b>{index + 1}</b></StructuredListCell>
               <StructuredListCell>{symptom.symptom}</StructuredListCell>
