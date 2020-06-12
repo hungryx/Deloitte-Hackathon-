f35f3c4b455a7361105c9b365f359a387eda66c2
Jessica Feng <Jessica.Feng101@gmail.com>
2020-06-12 21:15:08 +1000

    navigation bar

--- codedoc/src/App.js
+++ codedoc/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import 'carbon-components/css/carbon-components.min.css'
 import { Welcome } from './screens'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <Welcome />
-    </div>
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <div className="App">
+          <Welcome />
+        </div>
+      </Router>
+    </React.Fragment>
+    
   );
 }
 
