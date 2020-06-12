7b4aef07a9f438a385b963a2eaaddaeefbd76dd8
Marcus Lo <m.lo@student.unsw.edu.au>
2020-06-12 20:52:18 +1000

    notifs

--- codedoc/src/components/Medication.js
+++ codedoc/src/components/Medication.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react'
 import { Toggler } from './Components'
+import { ToastNotification } from 'carbon-components-react'
 
 
 export const Medication = ({ medication }) => {
+  const notifications = [
+    {
+      title: `You have 1 week's worth of Perindopril left.`,
+      caption: `Don't forget to visit your pharmacy.`
+    },
+    {
+      title: `You have 1 prescription left for Perindopril.`,
+      caption: `Visit your GP to renew your precsriptions.`
+    },
+    {
+      title: `You have 1 week's worth of Perindopril left.`,
+      caption: `Don't forget to visit your pharmacy.`
+    }
+  ]
   return (
     <div style={{ marginTop: 24 }}>
       <h4>Have you taken your daily medication?</h4>
+      <div style={{
+        display: 'flex',
+        overflow: 'scroll'
+      }}>
+        {notifications.map(notif => (
+          <ToastNotification
+            style={{ minWidth: 300 }}
+            lowContrast
+            kind={'warning'}
+            title={notif.title}
+            caption={notif.caption}
+          />
+        ))}
+      </div>
+
+
       <div style={{ marginTop: 12 }}>
         {medication.map(med => {
           return (
--- codedoc/src/components/Symptoms.js
+++ codedoc/src/components/Symptoms.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FormGroup, TextArea } from 'carbon-components-react'
 import { Toggler } from './Components'
 
 
@@ -12,6 +13,17 @@ export const Symptoms = ({ symptoms }) => {
             <Toggler label={symptom.label} color='#CD5C5C' />
           )
         })}
+      </div>
+      <div style={{ marginTop: 24 }}>
+        <h4>Additional Notes</h4>
+        <FormGroup>
+          <TextArea
+            style={{
+              border: '2px solid #FFC222'
+            }}
+            placeholder="Today I felt..."
+          />
+        </FormGroup>
       </div>
     </div>
   )
